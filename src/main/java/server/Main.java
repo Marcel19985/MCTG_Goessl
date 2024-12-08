d6ad135fbe2ec0128c6a32752ec4cd263e23cb15
Marcel Gössl <if23b093@technikum-wien.at>
2024-12-08 16:12:28 +0100

    Thread Pool added

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -3,16 +3,21 @@ package server;
 //Klassen für Netzwerkkommunikation:
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService; //Thread Pool
+import java.util.concurrent.Executors; //Thread Pool
 
 public class Main {
     public static void main(String[] args) {
+
+        ExecutorService threadPool = Executors.newFixedThreadPool(10);
+
         try (ServerSocket serverSocket = new ServerSocket(10001)) {  //erstellt ServerSocket, der auf Port 10001 hört; Klasse ServerSocket hat einen Konstruktur, der den Port 10001 (reservierter Port, sollte verfügbar sein) öffnet
             //Block wird nur ausgeführt wenn Bedingung in try true ist:
             System.out.println("Server started on port 10001, waiting for connections...");
 
             while (true) {
                 Socket clientSocket = serverSocket.accept(); //Sobald Client Verbunden ist, wird Socket Objekt zurück gegeben
-                new Thread(() -> {
+                threadPool.submit(() -> {
                     try {
                         HttpServer httpServer = new HttpServer();
                         httpServer.handleClient(clientSocket); //ruft handleClient auf
@@ -25,11 +30,13 @@ public class Main {
                             e.printStackTrace();
                         }
                     }
-                }).start(); //start vom Thread
+                });
             }
 
         } catch (Exception e) {
             e.printStackTrace();  //bei Exception wird Fehler ausgegeben
+        } finally {
+            threadPool.shutdown(); // Schließt den Thread Pool beim Beenden des Programms
         }
     }
 }
